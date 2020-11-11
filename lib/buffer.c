openharmony/third_party_libfuse

commit b95aba823b036a113858c6c98adf117c96aaac23

buffer.c: check whether buf is NULL in fuse_bufvec_advance func
In fuse_bufvec_advance func, calling fuse_bufvec_current func
may return NULL, so we should check whether buf is NULL before
using it.

Signed-off-by: Zhiqiang Liu <liuzhiqiang26@huawei.com>
Signed-off-by: Haotian Liu <lihaotian9@huawei.com>

lib/buffer.c
@@ -266,6 +266,9 @@ static int fuse_bufvec_advance(struct fuse_bufvec *bufv, size_t len)
 {
 	const struct fuse_buf *buf = fuse_bufvec_current(bufv);
 
+	if (!buf)
+		return 1;
+
 	bufv->off += len;
 	assert(bufv->off <= buf->size);
 	if (bufv->off == buf->size) {

commit 3c5b51c3594e23c4f06e290127fce926808841ad

buffer.c: correct return value when buf is NULL
In commit b95aba82 ("buffer.c: check whether buf is
NULL in fuse_bufvec_advance func"), if fuse_bufvec_current
func returns NULL, it returns 1 directly. Actually,
we should return 0 when buf is NULL.

Fixes: b95aba82 ("buffer.c: check whether buf is NULL in fuse_bufvec_advance func")
Signed-off-by: Zhiqiang Liu <liuzhiqiang26@huawei.com>
Signed-off-by: Haotian Li <lihaotian9@huawei.com>

lib/buffer.c
@@ -267,7 +267,7 @@ static int fuse_bufvec_advance(struct fuse_bufvec *bufv, size_t len)
 	const struct fuse_buf *buf = fuse_bufvec_current(bufv);
 
 	if (!buf)
-		return 1;
+		return 0;
 
 	bufv->off += len;
 	assert(bufv->off <= buf->size);